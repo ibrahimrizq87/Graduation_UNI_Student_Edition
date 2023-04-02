b06e43f0dd5ab41170a281d311a8fb94160f547b
Ibrahim Rizq <61823561+ibrahimrizq87@users.noreply.github.com>
2023-04-02 17:44:21 +0200

    sign up last update

--- app/src/main/java/com/uni/unistudent/ui/HomeScreen.kt
+++ app/src/main/java/com/uni/unistudent/ui/HomeScreen.kt
@@ -3,10 +3,16 @@ package com.uni.unistudent.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.uni.unistudent.R
+import com.uni.unistudent.databinding.ActivityHomeScreenBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeScreen : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_screen)
+        binding = ActivityHomeScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
