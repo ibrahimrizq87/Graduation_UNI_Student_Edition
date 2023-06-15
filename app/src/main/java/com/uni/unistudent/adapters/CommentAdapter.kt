94977c35ee4739d60cd70a74188be876271ea888
walidatef <waliddatef@gmail.com>
2023-06-15 11:14:56 +0300

    Create comment view

--- app/src/main/java/com/uni/unistudent/adapters/CommentAdapter.kt
+++ app/src/main/java/com/uni/unistudent/adapters/CommentAdapter.kt
@@ -2,10 +2,13 @@ package com.uni.unistudent.adapters
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.uni.unistudent.R
@@ -16,35 +19,37 @@ import com.uni.unistudent.classes.Posts
 
 class CommentAdapter(
     val context: Context,
-    var commentList:MutableList<MyComments>,
-    val onUpdate:(Int, MyComments) ->Unit,
-    val onDelete:(Int, MyComments) ->Unit
+    var commentList: MutableList<MyComments>,
+    val onUpdate: (Int, MyComments) -> Unit,
+    val onDelete: (Int, MyComments) -> Unit
 
-    )
-    : RecyclerView.Adapter<CommentAdapter.myViewHolder>() {
-
+) : RecyclerView.Adapter<CommentAdapter.myViewHolder>() {
+    lateinit var instance: View
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): myViewHolder {
-        val view : View = LayoutInflater.from(context).inflate(R.layout.comment_item,parent,false)
+        val view: View = LayoutInflater.from(context).inflate(R.layout.comment_item, parent, false)
+
+
         return myViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: myViewHolder, position: Int) {
         val currentItem = commentList[position]
-        if (currentItem.myComment){
-            holder.delete_bt.visibility=View.VISIBLE
-            holder.update_bt.visibility=View.VISIBLE
+        if (currentItem.myComment) {
+            holder.delete_bt.visibility = View.VISIBLE
+            holder.update_bt.visibility = View.VISIBLE
         }
         holder.auth.text = currentItem.authorName
+        //todo ya ibrahem
+        holder.auth_id.text = "112222"
         holder.comment.text = currentItem.description
 
 
     }
-    fun update(list: MutableList<MyComments>){
-        this.commentList=list
+
+    fun update(list: MutableList<MyComments>) {
+        this.commentList = list
         notifyDataSetChanged()
     }
-
-
 
 
     override fun getItemCount(): Int {
@@ -52,24 +57,26 @@ class CommentAdapter(
     }
 
 
-    inner    class myViewHolder(item: View) : RecyclerView.ViewHolder(item){
+    inner class myViewHolder(item: View) : RecyclerView.ViewHolder(item) {
+
 
         val auth = item.findViewById<TextView>(R.id.author_comment)
+        val auth_id = item.findViewById<TextView>(R.id.author_id)
         val comment = item.findViewById<TextView>(R.id.comment_text)
-        val update_bt = item.findViewById<Button>(R.id.update_comment)
-        val delete_bt = item.findViewById<Button>(R.id.delete_comment)
+        val update_bt: ImageButton = item.findViewById(R.id.update_comment)
+        val delete_bt: ImageButton = item.findViewById(R.id.delete_comment)
 
         init {
+
+
             update_bt.setOnClickListener {
-                onUpdate.invoke(adapterPosition,commentList[adapterPosition])
+                onUpdate.invoke(adapterPosition, commentList[adapterPosition])
             }
             delete_bt.setOnClickListener {
-                onDelete.invoke(adapterPosition,commentList[adapterPosition])
+                onDelete.invoke(adapterPosition, commentList[adapterPosition])
             }
 
         }
-
-
 
 
     }
